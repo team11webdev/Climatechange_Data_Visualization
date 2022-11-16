98f93b3598fbef5dcdce752b6557077622538efb
eliastom <sunkenseafoam@gmail.com>
2022-11-16 11:59:02 +0200

    initial work on N3 started, no functionality or css

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "password",
+  password: "3rdTIMEcharm",
   database: "charttest",
 });
 
--- src/views/N3_User_Specific.js
+++ src/views/N3_User_Specific.js
@@ -1,18 +1,115 @@
-import V8 from '../components/V8_CO2_Country';
-import V9 from '../components/V9_CO2_Sector';
- 
+
 function N3_USER_SPECIFIC() {
+    var URL = "https://github.com/"
+    var customURL = "team11webdev"
+    var customID = 23
+
     return (
      <>
- 
- <h1>test n3</h1>
-  
+     
+     <div className='n3-view-container'>
 
+        <div className='created-custom-container'>
+            <h3>Created Custom Views</h3>
+            <div id={customID} className='created-custom-views'>
+                <div className='custom-view-info'>
+                    <div>{customID} - </div>
+                    <a href={URL + customURL}>{URL + customURL}</a>
+                    <div>Copy link</div>
+                    <div>Delete</div>
+                </div>
+                <div id={customID} className='custom-view-info'>
+                    <div>{customID} - </div>
+                    <a href={URL + customURL}>{URL + customURL}</a>
+                    <div>Copy link</div>
+                    <div>Delete</div>
+                </div>
+                <div id={customID} className='custom-view-info'>
+                    <div>{customID} - </div>
+                    <a href={URL + customURL}>{URL + customURL}</a>
+                    <div>Copy link</div>
+                    <div>Delete</div>
+                </div>
+            </div>
+
+        </div>
+
+        <div className="custom-view-creator-container">
+
+            <h3>Create Custom View</h3>
+            <form className="form-container">
+
+                <div className="v-container">
+                    <input type="checkbox" id="v1" name="v1" value="v1"></input>
+                    <label htmlFor="v1">V1</label>
+                    <input type="checkbox" id="v2" name="v2" value="v2"></input>
+                    <label htmlFor="v2">+ V2</label>
+                    <input type="text" id="custom-desc-v1" placeholder="Input Description"></input>
+                </div>
+
+                <div className="v-container">
+                    <input type="checkbox" id="v3" name="v3" value="v3"></input>
+                    <label htmlFor="v3">V3</label>
+                    <input type="checkbox" id="v4" name="v4" value="v4"></input>
+                    <label htmlFor="v4">+ V4</label>
+                    <input type="checkbox" id="v10" name="v10" value="v10"></input>
+                    <label htmlFor="v10">+ V10</label>
+                    <input type="text" id="custom-desc-v3" placeholder="Input Description"></input>
+                </div>
+
+                <div className="v-container">
+                    <input type="checkbox" id="v5" name="v5" value="v5"></input>
+                    <label htmlFor="v5">V5</label>
+                    <input type="text" id="custom-desc-v5" placeholder="Input Description"></input>
+                </div>
+
+                <div className="v-container">
+                    <input type="checkbox" id="v6" name="v6" value="v6"></input>
+                    <label htmlFor="v6">V6</label>
+                    <input type="text" id="custom-desc-v6" placeholder="Input Description"></input>
+                </div>
+
+                <div className="v-container">
+                    <input type="checkbox" id="v7" name="v7" value="v7"></input>
+                    <label htmlFor="v7">V7</label>
+                    <input type="checkbox" id="v10" name="v10" value="v10"></input>
+                    <label htmlFor="v10">+ V10</label>
+                    <input type="text" id="custom-desc-v7" placeholder="Input Description"></input>
+                </div>
+
+                <div className="v-container">
+                    <input type="checkbox" id="v8" name="v8" value="v8"></input>
+                    <label htmlFor="v8">V8</label>
+                    <input type="text" id="custom-desc-v8" placeholder="Input Description"></input>
+                </div>
+
+                <div className="v-container">
+                    <input type="checkbox" id="v9" name="v9" value="v9"></input>
+                    <label htmlFor="v9">V9</label>
+                    <input type="text" id="custom-desc-v9" placeholder="Input Description"></input>
+                </div>
+
+                <div className="column-toggle-container">
+                    <input type="radio" id="col-1" name="col-number" value="col-toggle"></input>
+                    <label htmlFor="col-1">One Column</label>
+                    <input type="radio" id="col-2" name="col-number" placeholder="col-toggle"></input>
+                    <label htmlFor="col-2">Two Columns</label>
+                </div>
+
+                <input type="submit" value="Create"></input>
+            </form>
+
+
+        </div>
+
+
+        <form className="user-deletor">
+        <input type="submit" value="Delete User"></input>
+        </form>
+
+     </div>
      </>
     );
   }
-
-  /* <V8/>
-       <V9/> */
   
   export default N3_USER_SPECIFIC;
