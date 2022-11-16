7b7fca83c23ff2d7112f7f7d80adc8f27dbcf993
eliastom <sunkenseafoam@gmail.com>
2022-11-16 10:30:16 +0200

    gitignore database.js test

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "3rdTIMEcharm",
+  password: "password",
   database: "charttest",
 });
 
